dec86bb449bb7189340d84c365118149b2190da6
fake-dp <fakecoko92@gmail.com>
2025-04-08 13:56:48 +0900

    test ui

--- app/page.js
+++ app/page.js
@@ -1,35 +1,55 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import { useState } from 'react';
 import 'react-quill-new/dist/quill.snow.css';
+import { useState, useRef, useMemo } from 'react';
+import styled from 'styled-components';
 
-// SSR 비활성화된 Quill
 const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
 
-export default function Home() {
-  const [content, setContent] = useState('');
+const isIOS =
+  typeof window !== 'undefined' &&
+  /iPhone|iPad|iPod/i.test(navigator.userAgent);
 
-  const handleChange = (value) => {
-    setContent(value);
-  };
+export default function QuillTestPage() {
+  const [content, setContent] = useState('');
+  const quillRef = useRef(null);
+
+  const modules = useMemo(
+    () => ({ toolbar: [['bold', 'italic', 'underline']] }),
+    []
+  );
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>ReactQuill 테스트</h1>
-      <ReactQuill
-        theme='snow'
+      <CustomEditor
+        ref={quillRef}
         value={content}
-        onChange={handleChange}
-        placeholder='내용을 입력하세요'
-        style={{ height: '300px', marginBottom: '20px' }}
+        onChange={setContent}
+        placeholder='내용을 입력해주세요'
+        modules={modules}
+        theme='snow'
+        style={{
+          height: '300px',
+          marginBottom: '40px',
+        }}
       />
-      <div>
-        <h3>현재 입력된 내용:</h3>
-        <div style={{ border: '1px solid #ccc', padding: '10px' }}>
-          {content}
-        </div>
-      </div>
+      <div>실시간 값:</div>
+      <pre>{content}</pre>
     </div>
   );
 }
+
+const CustomEditor = styled(ReactQuill)`
+  .ql-container {
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    background-color: #fff;
+  }
+  .ql-editor {
+    font-size: 16px;
+    min-height: 200px;
+    padding: 10px;
+    color: #333;
+  }
+`;
